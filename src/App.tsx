80850c2b5e505d7dae8d9867d228ccb080ddb126
EvertonPRCarvalho <evertooon03@gmail.com>
2023-01-09 21:49:21 -0300

    teste

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+type Repository = {
+  full_name: string;
+  description: string;
+}
+
+function App() {
+  const [respo]
+  return (
+    <h1>Hello world!</h1>
+  )
+}
+
+export default App
